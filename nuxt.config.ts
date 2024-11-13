67b971c5723f025e745a11d34e0eca42d842b563
doublado <nikolajjak@pm.me>
2024-11-13 08:58:13 +0100

    add Tailwind CSS integration and configure Nuxt.js modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@nuxtjs/tailwindcss',
+    'nuxtjs-naive-ui'
+  ],
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+  }
 })
