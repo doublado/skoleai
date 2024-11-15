db09d79a07ce6b0d95998e6d561bac37bc762aa3
doublado <nikolajjak@pm.me>
2024-11-15 16:00:38 +0100

    enhance loading experience by adding NMessageProvider and improving code readability

--- app.vue
+++ app.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { useThemeStore } from '@/stores/themeStore'
-  import { NSpin } from 'naive-ui'
+  import { ref, onMounted } from 'vue';
+  import { useThemeStore } from '@/stores/themeStore';
+  import { NSpin, NMessageProvider } from 'naive-ui';
 
-  const themeStore = useThemeStore()
-  const isDarkMode = ref(themeStore.isDarkMode)
-  const isLoading = ref(true)
+  const themeStore = useThemeStore();
+  const isDarkMode = ref(themeStore.isDarkMode);
+  const isLoading = ref(true);
 
-  // Set loading to false after mount to ensure all components are ready
+  // Simulate a loading screen to provide a smoother user experience on initial load
   onMounted(() => {
     setTimeout(() => {
-      isLoading.value = false
-    }, 500) // Adjust the delay to match loading time requirements
-  })
+      isLoading.value = false;
+    }, 500);
+  });
 </script>
 
 <template>
-  <div v-if="isLoading" class="loading-overlay" :class="[isDarkMode ? 'dark' : 'light']">
-    <n-spin size="large" />
-  </div>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <n-message-provider>
+    <div v-if="isLoading" class="loading-overlay" :class="[isDarkMode ? 'dark' : 'light']">
+      <n-spin size="large" />
+    </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </n-message-provider>
 </template>
 
 <style scoped>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,29 @@
 <script lang="ts" setup>
-  import { ref } from 'vue'
-  import { NConfigProvider, darkTheme, lightTheme } from 'naive-ui'
-  import { useThemeStore } from '@/stores/themeStore'
+  import { ref } from 'vue';
+  import { NConfigProvider, darkTheme, lightTheme } from 'naive-ui';
+  import { useThemeStore } from '@/stores/themeStore';
 
-  // Theme store references
-  const themeStore = useThemeStore()
+  const themeStore = useThemeStore();
+  const isDarkMode = ref(themeStore.isDarkMode);
 
-  // Reactive state for theme
-  const isDarkMode = ref(themeStore.isDarkMode)
-
-  // Theme overrides for light mode
   const lightThemeOverrides = ref<GlobalThemeOverrides>({
     common: {
       fontFamily: 'Josefin Sans, sans-serif',
-    }
-  })
+    },
+  });
 
-  // Theme overrides for dark mode
   const darkThemeOverrides = ref<GlobalThemeOverrides>({
     common: {
       fontFamily: 'Josefin Sans, sans-serif',
-    }
-  })
+    },
+  });
 </script>
 
 <template>
-  <n-config-provider :theme="isDarkMode ? darkTheme : null" :theme-overrides="isDarkMode ? darkThemeOverrides : lightThemeOverrides">
+  <n-config-provider
+    :theme="isDarkMode ? darkTheme : null"
+    :theme-overrides="isDarkMode ? darkThemeOverrides : lightThemeOverrides"
+  >
     <slot />
   </n-config-provider>
 </template>
